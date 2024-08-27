4d60f4f338479664f56a30f2be57c9c69ace5e43
Alessandro Falls <alessandrofoz@hotmail.com>
2024-08-27 15:20:10 -0300

    monto

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 import express from 'express'
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://alessandrofalls:1OQYnpq8bUppEsdl@clusteraws-sp.okevs.mongodb.net/?retryWrites=true&w=majority&appName=ClusterAWS-SP";
+
 const app = express()
+
+const users = []
 
 app.use(express.json())
 
 /* Route Requirements
     1. Rote type / HTTP Method
     2. Address
+
+    alessandrofalls
+    1OQYnpq8bUppEsdl
+
+    mongodb+srv://alessandrofalls:1OQYnpq8bUppEsdl@clusteraws-sp.okevs.mongodb.net/?retryWrites=true&w=majority&appName=ClusterAWS-SP
 */
 
 app.get('/users', (req, res) =>  {
-    res.send('Ok, deu bom')
+    res.status(200).json(users)
 
 })
 
 app.post('/users', (req, res) =>  {
     
     console.log(req.body)
+
+    users.push(req.body)
     
-    res.send('Ok, deu bom POST')
+    res.status(201).json(req.body)
 
 })
 
@@ -31,3 +43,30 @@ app.listen(3000)
     - Edit user
     - Delete user
 */
+
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
